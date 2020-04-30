b1da06814305abacdc8f0eae773163a8afde9c8e
practiceland <the.teepee.creeper@gmail.com>
2020-04-29 19:31:09 -0700

    React now rendering to screen

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src', 'public', 'js', 'main.js'),
+  entry: path.resolve(__dirname, 'src', 'public', 'js', 'main.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
