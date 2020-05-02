47954b59e2c20b53dafe4a6532e357fe29bf8055
marissaconner <marissafromtexas@gmail.com>
2020-05-02 12:23:33 -0700

    Defined an endpoint for retrieving data to render

--- src/app.js
+++ src/app.js
@@ -5,6 +5,12 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '..', 'dist')));
 
+app.get('/tour/:id', (req, res) => {
+  // This route will look up a tour by ID and send the relevant data.
+  res.writeHead(200);
+  res.end();
+});
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '..', 'index.html'));
 });
