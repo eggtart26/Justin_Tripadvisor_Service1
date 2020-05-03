0f7568025e9ce98c9d685228de3b298b3ce7b6c0
marissaconner <marissafromtexas@gmail.com>
2020-05-02 19:07:00 -0700

    Startup script conditionally seeds database
    - If it finds 100 rows in tours, then it won't seed the DB
    - If it finds less than 100 rows in tours, it's seedin' time.

--- server/index.js
+++ server/index.js
@@ -3,15 +3,12 @@ const http = require('http').Server(app);
 const models = require('./models');
 const seed = require('./seeddatabase');
 
-const sequelizeOptions = {
-  force: true,
-};
+const sequelizeOptions = { force: process.env.FORCE_TRUNCATE || false };
+
 // If force is true, Sequelize will drop all tables and re-add whatever is in models
 // when the server starts.  This option should be put to 'false' in production!
 
 app.set('sqlport', process.env.SQLPORT || 5432);
-
-
 app.listen(3000, () => {
   console.log('Express server listening on port 3000 woop.');
 });
@@ -19,7 +16,16 @@ app.listen(3000, () => {
 models.sequelize.sync(sequelizeOptions).then(() => {
   const server = http.listen(app.get('sqlport'), () => {
     console.log(`SQL server on ${server.address().port}`);
-    seed(models);
+
+    models.Tour.count()
+      .then((results) => {
+        if (results <= 100) {
+          seed(models);
+        } else {
+          console.log('\x1b[32m%s\x1b[0m', `${results} rows found in Tours table: database seed script will not run.`);
+        }
+      })
+      .catch((error) => console.error(error));
   });
 })
   .catch((error) => {
