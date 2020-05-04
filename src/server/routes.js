26420dff80e81b8ace37888e461c44128578034e
marissaconner <marissafromtexas@gmail.com>
2020-05-04 09:17:53 -0700

    Installing Enzyme, updating static file location

--- src/server/routes.js
+++ src/server/routes.js
@@ -25,7 +25,7 @@ app.get('/tour/:id', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '..', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', '..', 'index.html'));
 });
 
 module.exports = app;
