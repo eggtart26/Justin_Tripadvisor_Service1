e1fc12868479e8464ad6f9099a96168c154b9526
marissaconner <marissafromtexas@gmail.com>
2020-05-04 12:49:59 -0700

    tests whether renders successfully

--- src/public/js/POIEntry.test.js
+++ src/public/js/POIEntry.test.js
@@ -1,7 +1,8 @@
+import React from 'react';
+import { shallow } from 'enzyme';
 import POIEntry from './POIEntry';
 
-describe('Addition', () => {
-  it('knows that 2 and 2 make 4', () => {
-    expect(2 + 2).toBe(4);
-  });
+test('It should render the POIEntry component to the screen', () => {
+  const wrapper = shallow(<POIEntry />);
+  expect(wrapper).toExist();
 });
