e28b3607e138fd48998801d43699040b3631d44a
marissaconner <marissafromtexas@gmail.com>
2020-05-06 10:28:29 -0700

    Individualized pins

--- src/public/js/GoogleMap.jsx
+++ src/public/js/GoogleMap.jsx
@@ -9,7 +9,7 @@ class GoogleMap extends Component {
 
   componentDidMount() {
     const googleScript = document.createElement('script')
-    googleScript.src = `https://maps.googleapis.com/maps/api/js?key=${"sprinkles"}&libraries=places`;
+    googleScript.src = `https://maps.googleapis.com/maps/api/js?key=${apikey}&libraries=places`;
     window.document.body.appendChild(googleScript)
     googleScript.addEventListener('load', ()=> {
       this.googleMap = this.createGoogleMap();
@@ -23,7 +23,6 @@ class GoogleMap extends Component {
       lng: attractions[0].longitude
     };
 
-
     var map = new window.google.maps.Map(this.googleMapRef.current, {
       zoom: 11,
       center: centerOn,
@@ -35,17 +34,19 @@ class GoogleMap extends Component {
         position: {
           lat: attractions[i].latitude,
           lng: attractions[i].longitude,
+          stopindex: i,
         },
         map: map,
+      });
+
+      pin.addListener('click', function() {
+        map.setZoom(14);
+        map.setCenter(this.getPosition());
       });
     }
   }
 
-
-
   render() {
-
-    
     return (
       <div
         id="google-map"
