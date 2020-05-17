b53206ef52ae60762b1a3a053ea25eb5946a35bd
marissaconner <marissafromtexas@gmail.com>
2020-05-16 20:17:06 -0700

    removing file-loader from webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path = require('path');
-const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src', 'public', 'components', 'main.jsx'),
@@ -22,18 +21,7 @@ module.exports = {
           'sass-loader',
         ],
       },
-      {
-        test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'fonts/',
-            },
-          },
-        ],
-      },
+      
     ],
   },
   mode: 'development',
